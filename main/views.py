c910af83dfdce1bf06d818fbb7733f80fd353c8d
Kim Sang Hyun <gocompu21@gmail.com>
2025-12-04 15:05:06 +0900

    4 commit

--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 blog_list_db = [
     {
         "id": 1,
-        "title": "장고는 너무 재밌어. 그래 계속해",
+        "title": "상현는 너무 재밌어. 그래 계속해",
         "content": "내용입니다.",
         "author": "user1",
     },
